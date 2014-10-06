79b107cd2e67e6149559f1b284f497e40c57279f
Mohit Mamoria <mohit.mamoria@gmail.com>
2014-10-06 13:21:10 +0530

    added all supervisors

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,6 +8,15 @@ template '/etc/supervisor/conf.d/pipe.conf' do
 end
 
 workers = {
+	'activities' => 1,
+	'default' => 1,
+	'events' => 1,
+	'hornlets' => 1,
+	'horns' => 1,
+	'mailets' => 1,
+	'mails' => 1,
+	'profiles' => 1,
+	'responses' => 1,
 	'campaigns' => 5,
 	'segments' => 5
 }
@@ -18,8 +27,11 @@ workers.each { |worker, count|
     	variables({
     		:worker_name => worker,
     		:worker_count => count,
-    		:worker_env => 'production'
+    		:worker_listen => (worker == 'default')
     	})
     end
 }
 
+execute 'reloading supervisor' do
+	command 'sudo supervisorctl reload'
+end
