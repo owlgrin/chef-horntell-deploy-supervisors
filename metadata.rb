caaa38b8d641eb63b85118749fd1f0caa85d3804
Mohit Mamoria <mohit.mamoria@gmail.com>
2014-10-06 12:43:14 +0530

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'horntell-supervisors'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures horntell-supervisors'
+long_description 'Installs/Configures horntell-supervisors'
+version          '0.1.0'
+
