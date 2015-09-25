63dc9854030af62cbba7a719cb7d7ea42fba24b8
Mohit Mamoria <mohit.mamoria@gmail.com>
2015-09-25 20:03:11 +0530

    fixed the worker repo

--- recipes/apps.rb
+++ recipes/apps.rb
@@ -6,6 +6,7 @@ workers.each { |worker, count|
 	template "/etc/supervisor/conf.d/worker-apps-#{worker}.conf" do
 		source 'worker.erb'
 		variables({
+			:repo_name => 'apps',
 			:worker_name => "apps:#{worker}",
 			:worker_count => count,
 			:worker_listen => false
