b3104b922fbd83b390dc9953c5edb4574cdaa64f
Mohit Mamoria <mohit.mamoria@gmail.com>
2015-09-25 20:12:16 +0530

    create new template for apps worker

--- recipes/apps.rb
+++ recipes/apps.rb
@@ -4,7 +4,7 @@ workers = {
 
 workers.each { |worker, count|
 	template "/etc/supervisor/conf.d/worker-apps-#{worker}.conf" do
-		source 'worker.erb'
+		source 'apps-worker.erb'
 		variables({
 			:repo_name => 'apps',
 			:worker_name => "apps:#{worker}",
